7d0e8c04f9bfc35eff6a2f58284257697b6b266e
mateus-sartorio <mateussartorio39@gmail.com>
2024-05-27 22:19:45 -0300

    initial setup

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
 
+  app.get("/api/convert", (request, response, next) => {});
 };
